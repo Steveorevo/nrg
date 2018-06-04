f1cebf103fe5a681a2cb2666c32c1f01e6b7c58c
Stephen J. Carnam <steve@steveorevo.com>
2018-06-03 20:41:21 -0700

    Subproject support

---
+++ manifest.js
@@ -0,0 +1,63 @@
+/**
+ * This code is machine generated.
+ */
+module.exports = function(RED) {
+  if (typeof RED.subprojects != 'undefined') {
+    RED.subprojects.manifests.push(
+      {
+        "subflows": [
+          {
+            "name": "nrg",
+            "id": "2b744329.f2992c",
+            "checked": "checked"
+          }
+        ],
+        "files": [
+          {
+            "isDirectory": "true",
+            "name": "demo",
+            "path": "~/projects/nrg/demo",
+            "id": "demo",
+            "checked": ""
+          },
+          {
+            "isDirectory": "false",
+            "name": "nrg-config.jpg",
+            "path": "~/projects/nrg/demo/nrg-config.jpg",
+            "id": "demo-nrg-config-jpg",
+            "checked": ""
+          },
+          {
+            "isDirectory": "false",
+            "name": "nrg-hello-world.jpg",
+            "path": "~/projects/nrg/demo/nrg-hello-world.jpg",
+            "id": "demo-nrg-hello-world-jpg",
+            "checked": ""
+          },
+          {
+            "isDirectory": "false",
+            "name": "nrg_cred.json",
+            "path": "~/projects/nrg/nrg_cred.json",
+            "id": "nrg-cred-json",
+            "checked": ""
+          },
+          {
+            "isDirectory": "false",
+            "name": "nrg.json",
+            "path": "~/projects/nrg/nrg.json",
+            "id": "nrg-json",
+            "checked": ""
+          }
+        ],
+        "tabs": [
+          {
+            "label": "Example",
+            "id": "1c7dd13.e21a82f",
+            "checked": "checked"
+          }
+        ],
+        "project": "nrg"
+      }
+    );
+  }
+};
